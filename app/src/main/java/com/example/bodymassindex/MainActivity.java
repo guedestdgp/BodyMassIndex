d53512d068672086f7e6810b776a5688d5c8793e
Tatiane <guedestdgp@outlook.com>
2019-11-26 13:24:36 -0400

    Initial commit

---
+++ app/src/main/java/com/example/bodymassindex/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.bodymassindex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText heightField = findViewById(R.id.height);
+    EditText massField = findViewById(R.id.mass);
+    Button btn = findViewById(R.id.btn);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double heightCalc = Double.valueOf(heightField.getText().toString());
+                heightCalc = Double.valueOf(massField.getText().toString())/ heightCalc;
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                intent.putExtra("bmi", Double.toString(heightCalc));
+            }
+        });
+    }
+}
